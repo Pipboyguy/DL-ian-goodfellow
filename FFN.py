a9240119d550ce38944a8e4c51f47fb5ac4582c9
Marcel Coetzee <coetzee.marcel2@gmail.com>
2020-11-07 19:56:01 +0200

    MLP implementation framework added

---
+++ FFN.py
@@ -0,0 +1,17 @@
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+
+
+class FullyConnectedMLP(object):
+
+    def __init__(self, input_shape,num_layers: int, layer_shape, output_shape):
+        pass
+    
+    def forward_propagation(self):
+        """
+        ***********************************************************
+        Algorithm 6.3 - Forward Propagtation
+
+        ***********************************************************
+        """
+        pass
